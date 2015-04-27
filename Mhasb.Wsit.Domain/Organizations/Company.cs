4ac30055d38ec468f796e862071f57c8c456a239
sunnoSakib <sunno.sakib@gmail.com>
2015-04-27 17:45:58 +0600

    Company Domain

---
+++ Mhasb.Wsit.Domain/Organizations/Company.cs
@@ -0,0 +1,76 @@
+﻿using Mhasb.Wsit.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mhasb.Domain.Organizations
+{
+    public class Company : IObjectStateInt
+    {
+        public int Id { get; set; }
+
+        public long UserId { get; set; }
+
+        [Required(ErrorMessage = "Trading Name is required")]
+        [StringLength(100)]
+        public string TradingName { get; set; }
+
+        [StringLength(100)]
+        public string LegalName { get; set; }
+
+        [StringLength(100)]
+        public string DisplayName { get; set; }
+
+        [Required(ErrorMessage = "Industry is required")]
+        public int IndustryId { get; set; }
+
+        [Required(ErrorMessage = "Country is required")]
+        public int CountryId { get; set; }
+
+        [Required(ErrorMessage = "Language is required")]
+        public int LanguageId { get; set; }
+
+        [StringLength(50)]
+        public string Tel { get; set; }
+
+        [StringLength(50)]
+        public string Fax { get; set; }
+        public int P_O_Box { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [StringLength(50)]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Location is required")]
+        [StringLength(200)]
+        public string Location { get; set; }
+
+        [StringLength(50)]
+        public string Website { get; set; }
+
+        public int LegalEntityId { get; set; }
+
+        public int TimezoneId { get; set; }
+
+        public string LogoLocation { get; set; }
+
+        public string SealLocation { get; set; }
+
+        public string DocumentLocation { get; set; }
+        
+        //[Required(ErrorMessage = "Password is required")]
+        //[StringLength(50, ErrorMessage = "Must be between 5 and 50 characters", MinimumLength = 5)]
+        //[DataType(DataType.Password)]
+        //public string Password { get; set; }
+
+   
+        public ObjectState State { get; set; }
+       
+    }
+}
---
+++ Mhasb.Wsit.Domain/Organizations/CompanyProfile.cs
@@ -0,0 +1,66 @@
+﻿using Mhasb.Wsit.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mhasb.Domain.Organizations
+{
+    public class CompanyProfile : IObjectStateInt
+    {
+        public int Id { get; set; }
+
+        public int CompanyId { get; set; }
+
+        public long UserId { get; set; }
+
+        [StringLength(100)]
+        public string BusinessName { get; set; }
+
+        [StringLength(1000)]
+        public string Vision { get; set; }
+
+        public int TurnOver { get; set; }
+
+        [StringLength(1000)]
+        public string Objectives { get; set; }
+
+        [StringLength(1000)]
+        public string Mision { get; set; }
+
+        [StringLength(1000)]
+        public string Experties { get; set; }
+
+        [StringLength(1000)]
+        public string Activities { get; set; }
+
+        [StringLength(1000)]
+        public string Markets { get; set; }
+
+        [StringLength(1000)]
+        public string PreviousWork { get; set; }
+
+        [StringLength(1000)]
+        public string Address { get; set; }
+
+        public int Location { get; set; }
+
+        [StringLength(50)]
+        public string Phone { get; set; }
+
+        [StringLength(50)]
+        public string Website { get; set; }
+
+        //[Required(ErrorMessage = "Email is required")]
+        [StringLength(50)]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public ObjectState State { get; set; }
+
+    }
+}
