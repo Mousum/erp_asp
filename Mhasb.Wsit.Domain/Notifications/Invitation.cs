6437f95e25b96e6f7c28add56a4f337ea558379b
mahbubh <mahbub@workspaceit.com>
2015-04-27 19:30:42 +0600

    Subscription Folder Add

--- Mhasb.Wsit.Domain/Notifications/Invitation.cs
+++ Mhasb.Wsit.Domain/Notifications/Invitation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mhasb.Wsit.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,37 @@ using System.Threading.Tasks;
 
 namespace Mhasb.Domain.Notifications
 {
-    class Invitation
+   public class Invitation:IObjectStateLong
     {
+        public string Email { get; set; }
+        public long CompanyId { get; set; }
+
+        public EmpTypeEnum EmployeeType { get; set; }
+        public string Token { get; set; }
+        public StatusEnum Status { get; set; }
+
+        public DateTime SendDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+
+        public long Id { get; set; }
+
+        public ObjectState State { get; set; }
+        
     }
+   public enum EmpTypeEnum
+   {
+       test1 = 1,
+       test2 = 2,
+       test3 = 3,
+       test4 = 4
+   }
+   public enum StatusEnum
+   {
+       test1 = 1,
+       test2 = 2,
+       test3 = 3,
+       test4 = 4
+   }
+
 }
  
