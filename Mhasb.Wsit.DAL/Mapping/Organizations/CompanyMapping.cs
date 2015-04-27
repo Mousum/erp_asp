e784473cae2139bb64bcd12415438164d29bccda
sunnoSakib <sunno.sakib@gmail.com>
2015-04-27 18:26:04 +0600

    Company Mapping started

---
+++ Mhasb.Wsit.DAL/Mapping/Organizations/CompanyMapping.cs
@@ -0,0 +1,31 @@
+﻿using Mhasb.Domain.Organizations;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Mhasb.DAL.Mapping.Organizations
+{
+    public class CompanyMapping : EntityTypeConfiguration<Company>
+    {
+        public CompanyMapping()
+        {
+            // Key
+            this.HasKey(c => c.Id);
+            // ignor
+            this.Ignore(c => c.State);
+
+            this.Property(c => c.TradingName).HasMaxLength(100).HasColumnName("trading_name");
+            this.Property(c => c.LegalName).HasMaxLength(100).HasColumnName("trading_name");
+            this.Property(c => c.DisplayName).HasMaxLength(100).HasColumnName("display_name");
+            this.Property(c => c.Tel).HasMaxLength(100).HasColumnName("tel");
+
+
+            this.ToTable("com.company");
+
+        }
+    }
+}
