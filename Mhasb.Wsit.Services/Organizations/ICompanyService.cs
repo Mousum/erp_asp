e82d60636deaa581e4c414dffbdde8ab17589d75
sunnoSakib <sunno.sakib@gmail.com>
2015-04-29 15:20:52 +0600

    Company Service Added

---
+++ Mhasb.Wsit.Services/Organizations/ICompanyService.cs
@@ -0,0 +1,22 @@
+﻿using Mhasb.Domain.Organizations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mhasb.Services.Organizations
+{
+    public interface ICompanyService
+    {
+        bool AddCompany(Company company);
+
+        bool UpdateCompany(Company company);
+
+        bool DeleteCompany(int id);
+
+        Company GetSingleCompany(int id);
+
+        List<Company> GetAllCompanies();
+    }
+}
