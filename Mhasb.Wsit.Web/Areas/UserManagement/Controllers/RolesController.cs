7eb4d3f81333ec6875cf99e3f0e8b9b8e303ab0d
Mausum Nandy <mousumaiub10@gmail.com>
2015-04-30 15:39:25 +0600

    RoleService

---
+++ Mhasb.Wsit.Web/Areas/UserManagement/Controllers/RolesController.cs
@@ -0,0 +1,40 @@
+﻿using Mhasb.Domain.Users;
+using Mhasb.Services.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mhasb.Wsit.Web.Areas.UserManagement.Controllers
+{
+    public class RolesController :Controller
+    {
+        private IRoleService rService = new RoleService();
+
+        public ActionResult Create() {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Role role) 
+        {
+            rService.AddRole(role);
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Edit(Role  role)
+        {
+            rService.EditRole(role);
+            return View();
+        }
+
+
+    }
+}
