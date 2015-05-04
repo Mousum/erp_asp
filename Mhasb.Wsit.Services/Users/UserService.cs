5f2d2601fabfd5591e62aff9e20ee7dd32b0a3f9
Mausum Nandy <mousumaiub10@gmail.com>
2015-05-04 13:53:04 +0600

    User in Role

--- Mhasb.Wsit.Services/Users/UserService.cs
+++ Mhasb.Wsit.Services/Users/UserService.cs
@@ -75,5 +75,24 @@ namespace Mhasb.Services.Users
         
         }
 
+        public List<User> GetAllUsers() 
+        {
+            try
+            {
+            
+                var UserObj = userRep.GetOperation()
+                                    .Get() .ToList();
+
+                
+                return UserObj;
+
+            }
+            catch (Exception ex)
+            {
+                var rr = ex.Message;
+                return null;
+            }
+        }
+
     }
 }
