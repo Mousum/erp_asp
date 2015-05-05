96d861f3de4c92da5b14ff5f68c89e708398bb0e
russelsikder <shsikder08@gmail.com>
2015-05-05 18:58:35 +0600

    russel

--- Mhasb.Wsit.Web/Controllers/HomeController.cs
+++ Mhasb.Wsit.Web/Controllers/HomeController.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Mhasb.Wsit.Web.Areas.UserManagement.Models;
 using Mhasb.Services.Users;
+using Mhasb.Wsit.Web.Areas.UserManagement.Models;
 
 
 namespace Mhasb.Wsit.Web.Controllers
