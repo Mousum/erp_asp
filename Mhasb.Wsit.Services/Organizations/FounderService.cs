b65c243753a6f1bf691e0b164e396de1fba339ae
mahbubh <mahbub@workspaceit.com>
2015-05-14 14:08:26 +0600

    Founder Services done

---
+++ Mhasb.Wsit.Services/Organizations/FounderService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mhasb.Domain.Organizations;
+using Mhasb.Wsit.DAL.Operations;
+using Mhasb.Wsit.Domain;
+
+namespace Mhasb.Services.Organizations
+{
+   public class FounderService:IFounder
+    {
+       private readonly CrudOperation<Founder> _crudOperation = new CrudOperation<Founder>();
+        public bool AddFounder(Founder founder)
+        {
+            try
+            {
+                founder.State = ObjectState.Added;
+                _crudOperation.AddOperation(founder);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                var errorMsg = ex.Message;
+                return false;
+            }
+        }
+
+        public bool UpdateFounder(Founder founder)
+        {
+            try
+            {
+                var dbObj = _crudOperation.GetSingleObject(founder.Id);
+                dbObj.FounderName = founder.FounderName;
+                dbObj.Email = founder.Email;
+                dbObj.Fax = founder.Fax;
+                dbObj.SharesOwned = founder.SharesOwned;
+                dbObj.TotalSharesValue = founder.TotalSharesValue;
+                dbObj.FounderResidence = founder.FounderResidence;
+                
+                dbObj.State = ObjectState.Modified;
+                _crudOperation.UpdateOperation(dbObj);
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                var errorMsg = ex.Message;
+                return false;
+            }
+        }
+
+        public List<Founder> GetFounders(int companyId)
+        {
+            var objList = _crudOperation.GetOperation()
+                .Include(d => d.Companies)
+                .Include(f => f.Countries)
+                .Filter(f => f.CompanyId == companyId)
+                .Get().ToList();
+            return objList;
+        }
+    }
+}
