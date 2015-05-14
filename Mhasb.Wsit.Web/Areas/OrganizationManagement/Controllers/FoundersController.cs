ba35bcb073360b3c4dd7dc6ed6bc4f1294ae45ce
Mausum Nandy <mousumaiub10@gmail.com>
2015-05-14 15:08:20 +0600

    bug solve

---
+++ Mhasb.Wsit.Web/Areas/OrganizationManagement/Controllers/FoundersController.cs
@@ -0,0 +1,38 @@
+﻿using Mhasb.Domain.Commons;
+using Mhasb.Domain.Organizations;
+using Mhasb.Domain.Users;
+using Mhasb.Services.Commons;
+using Mhasb.Services.Organizations;
+using Mhasb.Services.Users;
+using Mhasb.Wsit.CustomModel.Organizations;
+using Mhasb.Wsit.Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mhasb.Wsit.Web.Areas.OrganizationManagement.Controllers
+{
+    public class FoundersController : Controller
+    {
+        private readonly ICountryService iCountry = new CountryService();
+        private readonly ILanguageService iLang = new LanguageService();
+        // GET: OrganizationManagement/Founders
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Settings() 
+        {
+
+            ViewBag.CountryList = new SelectList(iCountry.GetAllCountries(), "Id", "CountryName");
+            ViewBag.LanguageList = new SelectList(iLang.GetAllLanguages(), "Id", "LanguageName");
+            return View();
+        
+        }
+    }
+}
