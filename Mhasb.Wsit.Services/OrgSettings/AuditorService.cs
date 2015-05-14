1a3837b4610be96263e200af67776f1936378750
sunnoSakib <sunno.sakib@gmail.com>
2015-05-14 12:52:06 +0600

    Auditor

---
+++ Mhasb.Wsit.Services/OrgSettings/AuditorService.cs
@@ -0,0 +1,34 @@
+﻿using Mhasb.Domain.OrgSettings;
+using Mhasb.Wsit.DAL.Operations;
+using Mhasb.Wsit.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mhasb.Services.OrgSettings
+{
+    public class AuditorService : IAuditor
+    {
+
+
+
+        private readonly CrudOperation<Auditor> auditorRep = new CrudOperation<Auditor>();
+        public bool AddAuditor(Auditor auditor)
+        {
+            try
+            {
+                auditor.State = ObjectState.Added;
+                auditorRep.AddOperation(auditor);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                var rr = ex.Message;
+                return false;
+            }
+
+        }
+    }
+}
