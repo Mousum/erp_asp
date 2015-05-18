8a686e2b80fe5a01016498a659bba5ed85c167f8
sunnoSakib <sunno.sakib@gmail.com>
2015-05-18 12:16:39 +0600

    Voucher Type Added

---
+++ Mhasb.Wsit.Services/Accounts/VoucherTypeService.cs
@@ -0,0 +1,34 @@
+﻿using Mhasb.Domain.Accounts;
+using Mhasb.Wsit.DAL.Operations;
+using Mhasb.Wsit.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mhasb.Services.Accounts
+{
+    public class VoucherTypeService : IVoucherType
+    {
+
+        private readonly CrudOperation<VoucherType> voucherRep = new CrudOperation<VoucherType>();
+        public bool AddVoucherType(VoucherType voucherType)
+        {
+            try
+            {
+                voucherType.State = ObjectState.Added;
+                voucherRep.AddOperation(voucherType);
+                voucherType.State = ObjectState.Unchanged;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                var rr = ex.Message;
+                return false;
+            }
+
+        }
+
+    }
+}
---
+++ Mhasb.Wsit.Web/Areas/Accounts/Controllers/VoucherTypeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mhasb.Wsit.Web.Areas.Accounts.Controllers
+{
+    public class VoucherTypeController : Controller
+    {
+        //
+        // GET: /Accounts/VoucherType/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+	}
+}
