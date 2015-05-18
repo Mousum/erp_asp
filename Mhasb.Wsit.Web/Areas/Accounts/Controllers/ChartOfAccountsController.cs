838aa3f5584d4d7ae81203a465a80c792756f707
Mausum Nandy <mousumaiub10@gmail.com>
2015-05-18 15:06:12 +0600

    chartofaccounts controller

--- Mhasb.Wsit.Web/Areas/Accounts/Controllers/ChartOfAccountsController.cs
+++ Mhasb.Wsit.Web/Areas/Accounts/Controllers/ChartOfAccountsController.cs
@@ -1,5 +1,5 @@
-﻿using Mhasb.Domain.OrgSettings;
-using Mhasb.Services.OrgSettings;
+﻿using Mhasb.Domain.Accounts;
+using Mhasb.Services.Accounts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +7,7 @@ using System.Web;
 using System.Web.Mvc;
 
 
-namespace Mhasb.Wsit.Web.Areas.OrgSettings.Controllers
+namespace Mhasb.Wsit.Web.Areas.Accounts.Controllers
 {
     public class ChartOfAccountsController : Controller
     {
