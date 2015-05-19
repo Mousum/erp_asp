c600f8fe8f24fe36766ff2478531c1325d4f5c31
sunnoSakib <sunno.sakib@gmail.com>
2015-05-19 16:08:19 +0600

    Voucher View

---
+++ Mhasb.Wsit.Web/Areas/Accounts/Controllers/VoucherController.cs
@@ -0,0 +1,38 @@
+﻿using Mhasb.Services.OrgSettings;
+using Mhasb.Wsit.CustomModel.Accounts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mhasb.Wsit.Web.Areas.Accounts.Controllers
+{
+    public class VoucherController : Controller
+    {
+        private readonly ICurrency cService = new CurrencyService();
+        private readonly ICurrency cService = new CurrencyService();
+        //
+        // GET: /Accounts/Voucher/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Create()
+        {
+            ViewBag.CurrencyList = new SelectList(cService.GetAllCurrency(), "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(VoucherCustom vc)
+        {
+            VoucherCustom v = vc;
+
+            return Content("sdfsd");
+        }
+
+	}
+}
