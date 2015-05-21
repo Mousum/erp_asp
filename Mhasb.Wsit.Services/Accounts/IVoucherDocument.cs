b195cf31c928639d0d778f4f1bb3b0794d87fd18
mahbubh <mahbub@workspaceit.com>
2015-05-21 11:41:39 +0600

    Voucher Document Service Done

---
+++ Mhasb.Wsit.Services/Accounts/IVoucherDocument.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mhasb.Domain.Accounts;
+
+namespace Mhasb.Services.Accounts
+{
+   public interface IVoucherDocument
+    {
+        bool AddDocument(VoucherDocument voucherDocument);
+        List<VoucherDocument> GetDocumentByVoucherId(long voucherId);
+    }
+}
---
+++ Mhasb.Wsit.Services/Accounts/VoucherDocumentService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mhasb.Domain.Accounts;
+using Mhasb.Wsit.DAL.Operations;
+using Mhasb.Wsit.Domain;
+
+namespace Mhasb.Services.Accounts
+{
+   public class VoucherDocumentService : IVoucherDocument
+    {
+       private readonly CrudOperation<VoucherDocument> _crudOperation = new CrudOperation<VoucherDocument>();
+        public bool AddDocument(VoucherDocument voucherDocument)
+        {
+
+            try
+            {
+                voucherDocument.State = ObjectState.Added;
+                _crudOperation.AddOperation(voucherDocument);
+                voucherDocument.State = ObjectState.Unchanged;
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                var rr = ex.Message;
+                return false;
+            }
+        }
+
+       public List<VoucherDocument> GetDocumentByVoucherId(long voucherId)
+       {
+           var vdList = _crudOperation.GetOperation()
+               .Filter(vd => vd.VoucherId == voucherId)
+               .Get().ToList();
+
+           return vdList;
+       }
+
+       public bool UpdateDocumentVoucher(VoucherDocument voucherDocument)
+       {
+
+
+           try
+           {
+               var dbObj = _crudOperation.GetSingleObject(voucherDocument.Id);
+
+               dbObj.Description = voucherDocument.Description;
+               voucherDocument.State = ObjectState.Modified;
+               _crudOperation.UpdateOperation(dbObj);
+               return true;
+           }
+           catch (Exception ex)
+           {
+
+               var rr = ex.Message;
+               return false;
+           }
+       }
+
+       public bool DeleteDocumentVoucher(long voucherDocId)
+       {
+           try
+           {
+               var dbObj = _crudOperation.GetSingleObject(voucherDocId);
+
+               dbObj.State = ObjectState.Deleted;
+               _crudOperation.DeleteOperation(voucherDocId);
+               return true;
+           }
+           catch (Exception ex)
+           {
+               var rr = ex.Message;
+               return false;
+           }
+       }
+    }
+}
