c877c23d3920abecf758b9f8b3c7a94d0cc8f8a8
sunnoSakib <sunno.sakib@gmail.com>
2015-05-25 18:26:35 +0600

    Share Transfer

---
+++ Mhasb.Wsit.Services/Organizations/IShareTransferService.cs
@@ -0,0 +1,14 @@
+﻿using Mhasb.Domain.Organizations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mhasb.Services.Organizations
+{
+    public interface IShareTransferService
+    {
+        bool AddShareTransferTransection(ShareTransfer st);
+    }
+}
