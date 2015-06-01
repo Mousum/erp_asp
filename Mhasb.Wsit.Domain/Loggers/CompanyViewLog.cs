2fbc4bff255b846ba1738b6429d43680e3dde04c
mahbubh <mahbub@workspaceit.com>
2015-06-01 16:57:22 +0600

    Logger Domain

---
+++ Mhasb.Wsit.Domain/Loggers/CompanyViewLog.cs
@@ -0,0 +1,27 @@
+﻿using Mhasb.Wsit.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mhasb.Domain.Organizations;
+using Mhasb.Domain.Users;
+
+namespace Mhasb.Domain.Loggers
+{
+    public class CompanyViewLog:IObjectStateLong
+    {
+
+        public long UserId { get; set; }
+        public int? CompanyId { get; set; }
+        public DateTime LoginTime { get; set; }
+        public string IpAddress { get; set; }
+        public long Id { get; set; }
+        public ObjectState State { get; set; }
+        
+        public virtual User Users { get; set; }
+
+        public virtual Company Companies { get; set; }
+
+    }
+}
