a15eb0fb2d028f9f7cd10f176555f24c46f86204
sunnoSakib <sunno.sakib@gmail.com>
2015-06-02 12:46:49 +0600

    Log View Domain

---
+++ Mhasb.Wsit.CustomModel/Organizations/LogView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mhasb.Wsit.CustomModel.Organizations
+{
+    public class LogView
+    {
+        int companyId { get; set; }
+        long userId { get; set; }
+        string companyName { get; set; }
+        DateTime lastViewedTime { get; set; }
+        string lastViewerFirstName { get; set; }
+        string lastViewerLastName { get; set; }
+        string roleName { get; set; }
+        string roleDescription { get; set; }
+        string subscriper { get; set; }
+        string subscriptionType { get; set; }
+
+    }
+}
