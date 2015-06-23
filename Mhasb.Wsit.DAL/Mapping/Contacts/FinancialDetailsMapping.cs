d19be689dd276a6beb2ae13057d97aae3278502b
russelsikder <shsikder08@gmail.com>
2015-06-23 11:44:42 -0700

    ContactServiceRaza

--- Mhasb.Wsit.DAL/Mapping/Contacts/FinancialDetailsMapping.cs
+++ Mhasb.Wsit.DAL/Mapping/Contacts/FinancialDetailsMapping.cs
@@ -16,18 +16,18 @@ namespace Mhasb.DAL.Mapping.Contacts
             this.Ignore(i => i.State);
             // Entities
             this.Property(i=>i.ContactInfoId).HasColumnName("contactinfoid");
-            this.Property(i => i.SalesDefaultTax).HasMaxLength(100).HasColumnName("salesdefaulttax");
-            this.Property(i => i.SalesDefaultAccount).HasMaxLength(100).HasColumnName("salesdefaultaccount");
-            this.Property(i => i.PurchasesDefaultTax).HasMaxLength(100).HasColumnName("purchasesdefaulttax");
-            this.Property(i => i.PurchasesDefaultAccount).HasMaxLength(100).HasColumnName("purchasesdefaultaccount");
-            this.Property(i => i.TaxNumber).HasMaxLength(100).HasColumnName("taxnumber");
-            this.Property(i => i.SalesTax).HasMaxLength(100).HasColumnName("salestax");
-            this.Property(i => i.PurchasesTax).HasMaxLength(100).HasColumnName("purchasestax");
-            this.Property(i => i.DefaultCurrency).HasMaxLength(100).HasColumnName("defaultcurrency");
-            this.Property(i => i.BankAccountNumber).HasMaxLength(100).HasColumnName("bankaccountnumber");
-            this.Property(i => i.BankAccountName).HasMaxLength(100).HasColumnName("bankaccountname");
+            this.Property(i => i.SalesDefaultTax).HasColumnName("salesdefaulttax");
+            this.Property(i => i.SalesDefaultAccount).HasColumnName("salesdefaultaccount");
+            this.Property(i => i.PurchasesDefaultTax).HasColumnName("purchasesdefaulttax");
+            this.Property(i => i.PurchasesDefaultAccount).HasColumnName("purchasesdefaultaccount");
+            this.Property(i => i.TaxNumber).HasMaxLength(150).HasColumnName("taxnumber");
+            this.Property(i => i.SalesTax).HasColumnName("salestax");
+            this.Property(i => i.PurchasesTax).HasColumnName("purchasestax");
+            this.Property(i => i.DefaultCurrency).HasColumnName("defaultcurrency");
+            this.Property(i => i.BankAccountNumber).HasColumnName("bankaccountnumber");
+            this.Property(i => i.BankAccountName).HasColumnName("bankaccountname");
             this.Property(i => i.Details).HasMaxLength(100).HasColumnName("details");
-            this.Property(i => i.BillsTerms).HasMaxLength(100).HasColumnName("billsTerms");
+            this.Property(i => i.BillsTerms).HasMaxLength(100).HasColumnName("billsterms");
             this.Property(i => i.SalesTerms).HasMaxLength(100).HasColumnName("salesterms");
             this.Property(i => i.NetworkKey).HasMaxLength(100).HasColumnName("networkKey");
             this.ToTable("con.FinancialDetails");
@@ -35,7 +35,30 @@ namespace Mhasb.DAL.Mapping.Contacts
             this.HasRequired(i => i.ContactInformations)
                 .WithMany()
                 .HasForeignKey(i=>i.ContactInfoId);
+            this.HasRequired(i => i.Lookups)
+                .WithMany()
+                .HasForeignKey(i => i.SalesDefaultTax);
+            this.HasRequired(i => i.Lookups)
+                .WithMany()
+                .HasForeignKey(i => i.PurchasesDefaultTax);
+            this.HasRequired(i => i.Lookups)
+                .WithMany()
+                .HasForeignKey(i => i.PurchasesTax);
+            this.HasRequired(i => i.Lookups)
+                .WithMany()
+                .HasForeignKey(i => i.SalesTax);
 
+            this.HasRequired(i => i.FinancialSettings)
+            .WithMany()
+            .HasForeignKey(i => i.SalesDefaultAccount);
+
+            this.HasRequired(i => i.FinancialSettings)
+            .WithMany()
+            .HasForeignKey(i => i.PurchasesDefaultAccount);
+
+            this.HasRequired(i => i.Currencies)
+           .WithMany()
+           .HasForeignKey(i => i.DefaultCurrency);
         }
     }
 }
---
+++ Mhasb.Wsit.Services/Contact/NotesService.cs
@@ -0,0 +1,90 @@
+﻿using Mhasb.Domain.Contacts;
+using Mhasb.Wsit.DAL.Operations;
+using Mhasb.Wsit.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mhasb.Services.Contact
+{
+    public class NotesService : INotesService
+    {
+        private readonly CrudOperation<Notes> _rep = new CrudOperation<Notes>();
+        public bool CreateNote(Notes note)
+        {
+            try { 
+             note.State=ObjectState.Added;
+                  _rep.AddOperation(note);
+             return true;
+            }catch(Exception ex){
+                var msg = ex.Message;
+                return false;
+            }
+        }
+
+        public bool UpdateNote(Notes note)
+        {
+            try
+            {
+                note.State = ObjectState.Modified;
+                _rep.UpdateOperation(note);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.Message;
+                return false;
+            }
+        }
+
+        public bool DeleteNOtes(long Id)
+        {
+            try
+            {
+               
+                _rep.DeleteOperation(Id);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.Message;
+                return false;
+            }
+        }
+
+        public List<Notes> getAllNotes()
+        {
+            try
+            {
+
+                var _obj = _rep.GetOperation()
+                    .Get().ToList();
+                return _obj;
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.Message;
+                return null;
+            }
+        }
+
+        public Notes GetNotesById(long Id)
+        {
+            try
+            {
+
+                var _obj = _rep.GetOperation()
+                    .Filter(i=>i.Id==Id)
+                    .Get().SingleOrDefault();
+                return _obj;
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.Message;
+                return null;
+            }
+        }
+    }
+}
