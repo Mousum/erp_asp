0492a8ab1b965821be41d258dead268eaa58f3ac
russelsikder <shsikder08@gmail.com>
2015-06-24 14:02:25 -0700

    Custom Mode for contact

---
+++ Mhasb.Wsit.CustomModel/Contact/ContactCustome.cs
@@ -0,0 +1,21 @@
+﻿using Mhasb.Domain.Contacts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mhasb.Wsit.CustomModel.Contact
+{
+    public class ContactCustome
+    {
+        public ContactInformation ContactInformation { get; set; }
+        public ContactDetails ContactDetails { get; set; }
+        public Person Person { get; set; }
+        public FinancialDetails FinancialDetails { get; set; }
+        public Notes Notes { get; set; }
+        public TelePhone TelePhone { get; set; }
+
+
+    }
+}
