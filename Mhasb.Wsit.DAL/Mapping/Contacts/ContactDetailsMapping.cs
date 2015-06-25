ca2d53b5e49a61ea5db116851dd4c18d06b1c3d8
sunnoSakib <sunno.sakib@gmail.com>
2015-06-25 16:32:39 +0600

    Contact layout

--- Mhasb.Wsit.DAL/Mapping/Contacts/ContactDetailsMapping.cs
+++ Mhasb.Wsit.DAL/Mapping/Contacts/ContactDetailsMapping.cs
@@ -21,7 +21,7 @@ namespace Mhasb.DAL.Mapping.Contacts
             this.Property(i => i.City).HasMaxLength(100).HasColumnName("city");
             this.Property(i => i.StateRegion).HasMaxLength(100).HasColumnName("stateregion");
             this.Property(i => i.ZipeCode).HasMaxLength(100).HasColumnName("zipecode");
-            this.Property(i => i.Country).HasColumnName("country");
+            this.Property(i => i.Country).HasMaxLength(100).HasColumnName("country");
             this.Property(i => i.Type).HasColumnName("type");
             this.ToTable("con.contactdetails");
 
