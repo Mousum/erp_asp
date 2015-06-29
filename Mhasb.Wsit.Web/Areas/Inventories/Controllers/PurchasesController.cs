5685e368c7859ae55d355b963eb41ec33960df32
Mausum Nandy <mousumaiub10@gmail.com>
2015-06-29 19:19:56 +0600

    Push

--- Mhasb.Wsit.Web/Areas/Inventories/Controllers/PurchasesController.cs
+++ Mhasb.Wsit.Web/Areas/Inventories/Controllers/PurchasesController.cs
@@ -1,4 +1,7 @@
-﻿using Mhasb.Services.Inventories;
+﻿using Mhasb.Domain.Contacts;
+using Mhasb.Services.Contact;
+using Mhasb.Wsit.DAL.Operations;
+using Mhasb.Services.Inventories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +12,7 @@ namespace Mhasb.Wsit.Web.Areas.Inventories
 {
     public class PurchasesController : Controller
     {
+        private IContactInformationService _repcontact = new ContactInformationService();
         private readonly IItemService itemService = new ItemService();
         //
         // GET: /Inventories/Purchases/
@@ -21,9 +25,15 @@ namespace Mhasb.Wsit.Web.Areas.Inventories
             return View();
         }
         public ActionResult RepeatTransection()
-        {
+         {
 
-            return View();
+             return View();
+         }
+        public JsonResult GetServiceNames(string term) {
+            var results = _repcontact.GetAllContactInformation().Where(s => term == null || s.ContactName.ToLower().Contains(term.ToLower())).Select(x => new { id = x.Id, value = x.ContactName }).Take(5).ToList();
+            
+            return Json(results, JsonRequestBehavior.AllowGet);
+          
         }
         public PartialViewResult ItemRow()
         {
@@ -32,5 +42,5 @@ namespace Mhasb.Wsit.Web.Areas.Inventories
             ViewBag.Items = new SelectList(Items, "Id", "Name");
             return PartialView();
         }
-    }
+	}
 }
