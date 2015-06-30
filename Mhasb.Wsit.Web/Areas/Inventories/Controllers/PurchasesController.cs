fabf9fc75f5233907c316bf5e697d6b7ea8edbc8
russelsikder <shsikder08@gmail.com>
2015-06-29 18:17:45 -0700

    RazaNewBill

--- Mhasb.Wsit.Web/Areas/Inventories/Controllers/PurchasesController.cs
+++ Mhasb.Wsit.Web/Areas/Inventories/Controllers/PurchasesController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Mhasb.Domain.Contacts;
+using Mhasb.Services.Contact;
+using Mhasb.Wsit.DAL.Operations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +11,10 @@ namespace Mhasb.Wsit.Web.Areas.Inventories
 {
     public class PurchasesController : Controller
     {
+
+        private IContactInformationService _repcontact = new ContactInformationService();
+
+
         //
         // GET: /Inventories/Purchases/
         public ActionResult Index()
@@ -17,6 +24,11 @@ namespace Mhasb.Wsit.Web.Areas.Inventories
         public ActionResult EditBill()
         {
             return View();
+        }
+        public JsonResult GetServiceNames(string term) {
+            var results = _repcontact.GetAllContactInformation().Where(s => term == null || s.ContactName.ToLower().Contains(term.ToLower())).Select(x => new { id = x.Id, value = x.ContactName }).Take(5).ToList();
+            
+            return Json(results, JsonRequestBehavior.AllowGet);
         }
 	}
 }
